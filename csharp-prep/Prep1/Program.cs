deefdebd1cd8f552868a92e733433652338cae42
ChadNuttall <chadnuttall@byui.edu>
2024-01-13 11:04:51 -0700

    Comments added Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,12 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Ask the user for their fist name
         Console.Write("What is you first name? ");
         string first = Console.ReadLine();
 
+        // Ask the user for their last name
         Console.Write("What is you last name? ");
         string last = Console.ReadLine();      
 
+        // Display message to user
         Console.WriteLine($"Your name is {last}, {first} {last}.");
 
     }
