faae6aabcb71a4d8d306d780068add2e1c286882
ChadNuttall <chadnuttall@byui.edu>
2024-01-24 11:59:28 -0700

    Prepare Resume and Job classes

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2015;
+        job1._endYear = 2016;
+        job1.DisplayJobDetails();
+
+        Job job2 = new Job();
+
+        job2._company = "Google";
+        job2._jobTitle = "Program Manager";
+        job2._startYear = 2020;
+        job2._endYear = 2024;
+        job2.DisplayJobDetails();
     }
 }
