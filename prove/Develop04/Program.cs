a7df211faea39db1ad58fc61043c374b0d2c09d0
ChadNuttall <chadnuttall@byui.edu>
2024-03-20 12:09:22 -0600

    Set up a User Menu

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        Console.WriteLine("Welcome to the Mindfulness Program!");
+        Console.WriteLine();
+        bool running = true;
+        while (running)
+        {
+            Console.WriteLine("Please select one of the following activities: ");
+            Console.WriteLine("1. Breathing Activity");
+            Console.WriteLine("2. Reflecting Activity");
+            Console.WriteLine("3. Listing Activity");
+            Console.WriteLine("4. Quit");
+            Console.WriteLine();
+            Console.Write("What would you like to do? ");
+            int choice = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            switch (choice)
+            {   
+                //Breathing
+                case 1:
+                    Console.WriteLine("Breathing Activity is under constuction.\n");
+                    break;
+                //Reflecting
+                case 2:
+                    Console.WriteLine("Reflecting Activity is under constuction.\n");
+                    break;
+                //Listing
+                case 3:
+                    Console.WriteLine("Listing Activity is under constuction.\n");
+                    break;
+                //Quit
+                case 4:
+                    running = false;
+                    Console.WriteLine("Have a nice day!");
+                    break;
+            }
+     
+        }
     }
 }
